284cac9000f37ace693f1e5018508022ad647480
Matthieu <Matthieu.cneude@gmail.com>
2018-03-30 10:44:08 +0200

    Install composer on CI

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,6 +8,11 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git -yqq
+apt-get install zip unzip -yqq
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php -yqq
+php -r "unlink('composer-setup.php');"
+php composer.phar install -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
