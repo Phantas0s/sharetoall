71dd1100138ae53a12c740470f458c13d0ec367e
matthieu <matthieu.cneude@gmail.com>
2018-04-25 20:29:57 +0200

    Fix the web folder problem

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$HTTP_AUTH_PASSWORD" != "" ]; then
+if [ "${HTTP_AUTH_PASSWORD}" != "" ]; then
   rm -rf /etc/nginx/.htpasswd
   echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -apr1 $HTTP_AUTH_PASSWORD) >> /etc/nginx/conf.d/.htpasswd
   echo "Basic auth is on for user ${HTTP_AUTH_LOGIN}..."
